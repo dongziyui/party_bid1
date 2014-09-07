21edf2b378af024e242c13e0abf813a505730331
dongziyui <810510707@qq.com>
2014-09-07 16:21:23 +0800

    报名列表数据存储

--- app/scripts/controllers/sms.js
+++ app/scripts/controllers/sms.js
@@ -2,85 +2,74 @@
 //
 
 var native_accessor = {
-    send_sms: function (phone, message) {
+        send_sms: function (phone, message) {
 //        native_access.send_sms({"receivers":[{"name":'name', "phone":phone}]}, {"message_content":message});
-        console.log(phone, message);
-    },
+            console.log(phone, message);
+        },
 
-    receive_message: function (json_message) {
-        if (typeof this.process_received_message === 'function') {
-            this.process_received_message(json_message);
-        }
-    },
+        receive_message: function (json_message) {
+            if (typeof this.process_received_message === 'function') {
+                this.process_received_message(json_message);
+            }
+        },
 
-    process_received_message: function (json_message) {
-        var activity = JSON.parse(localStorage.getItem("activity"))
-//        console.log(json_message.messages[0].message)
-        for (var j = 0; j< activity.length; j++) {
-            if (activity[j].status == 1) {
-                console.log("恭喜报名成功！")
-                json_message.messages[0].message.replace(/\s/g, "")
-                json_message.messages[0].message.search(/bm|BM/i)
-                if (i == 0) {
-                   var people_name=json_message.messages[0].message.substr(2)
-                   var phone_number=json_message.messages[0].phone
-                }
-                var join=JSON.parse(localStorage.getItem("join"))||[]
-                if(!join){
-                    join.unshift({
-                        name: people_name,
-                        phone: phone_number
-                    })
-                }
-                else {
-                    for (var j = 0; j < join.length; j++) {
-                        if(((people_name==join[j].name)||(people_name!=join[j].name))&&(phone_number!=join[j].name))
+        process_received_message: function (json_message) {
+            var activity = JSON.parse(localStorage.getItem("activity"))
+//
+            for (var j = 0; j < activity.length; j++) {
+                if (activity[j].status == 1) {
+                    console.log("恭喜报名成功！")
+                    json_message.messages[0].message.replace(/\s/g, "")
+                    var i
+                    json_message.messages[0].message.search(/bm|BM/i)
+                    if (i == 0) {
+                        var people_name = json_message.messages[0].message.substr(2)
+                        var phone_number = json_message.messages[0].phone
+                    }
+                    var join = JSON.parse(localStorage.getItem("join")) || []
+                    if (!join) {
                         join.unshift({
                             name: people_name,
                             phone: phone_number
                         })
                     }
+                    else {
+                        for (var j = 0; j < join.length; j++) {
+                            if (((people_name == join[j].name) || (people_name != join[j].name)) && (phone_number != join[j].name))
+                                join.unshift({
+                                    name: people_name,
+                                    phone: phone_number
+                                })
+                        }
+                    }
+                    localStorage.setItem('join', join)
+                    $scope.joins=JSON.parse(localStorage.getItem("join"))
+                    break
                 }
-                localStorage.setItem('join', join)
-                break
+                else {
+                    if (activity[j].status == 0) {
+                        var commit = true
+                    }
+                    else {
+                        commit = false
+                    }
+                }
             }
-        else
-        {
-            if (activity[j].status == 0) {
-                var commit = true
+
+            if (commit == true) {
+                console.log("活动尚未开始！")
+
             }
             else {
-                commit = false
+                console.log("报名已结束！")
             }
-        }
-        if (commit == true) {
-            console.log("活动尚未开始！")
+
 
         }
-        else {
-            console.log("报名已结束！")
-        }
+
+
     }
-}
-
-//                (activity[i].status==0){
-//                console.log("活动尚未开始！")
-//            }
-//            else
-//            }
-//            else{
-//                console.log("报名已结束！")
-//            }
-//
-//        }
-////        if()
-//        console.log(json_message)
-//
-//    }
-
-
-}
-;
+    ;
 
 function notify_message_received(message_json) {
     //console.log(JSON.stringify(message_json));
