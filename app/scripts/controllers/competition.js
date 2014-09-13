78d490883211396b560ef0cbf95940675f239022
dongziyui <810510707@qq.com>
2014-09-13 17:32:25 +0800

    竞价列表显示

--- app/scripts/controllers/competition.js
+++ app/scripts/controllers/competition.js
@@ -2,35 +2,75 @@
  * Created by dw on 14-9-13.
  */
 angular.module('partyBid1App')
- .controller('competitionCtrl',function($scope, $location){
+    .controller('competitionCtrl', function ($scope, $location) {
         var activity = JSON.parse(localStorage.getItem("activity"))
-        $scope.go_to_list=function(){
-        $location.path('/')
-    }
-        if (activity) {
-            for (var i = 0; i < activity.length; i++) {
-                if (activity[i].status == 1) {
+        var active_name = localStorage.getItem("action_name")
+        var bid_list = localStorage.getItem("bid_list") || []
+        $scope.bid_lists = localStorage.getItem("bid_list")
+        $scope.to_list = function () {
+            $location.path('/')
+        }
+
+
+        $scope.compete_action = function () {
+            var a = 0
+            if ($scope.start == "开始") {
+                $scope.start = "结束"
+                for (var i = 0; i < activity.length; i++) {
+
                     if (activity[i].name == active_name) {
-                        $scope.start_end = "结束"
+                        activity[i].status = 3
+                        localStorage.setItem('activity', JSON.stringify(activity));
+
                     }
-                    else {
-                        $scope.start_end = "开始"
-                        $scope.use = true
+                }
+                for (j = 0; j < activity.length; j++) {
+                    if (activity[j].status == 3) {
+                        a++
+                        console.log(a)
                     }
-                    var unable = true
-                    break
                 }
-                else {
-                    unable = false
-                }
-                if (unable == false) {
-                    $scope.start_end = "开始"
-                    $scope.use = false
+                bid_list.unshift({
+                    name: "竞价" + a,
+                    actname: active_name
+                })
+                localStorage.setItem('bid_list', JSON.stringify(bid_list));
+            }
+            else {
+                for (var i = 0; i < activity.length; i++) {
+                    if (activity[i].name == active_name) {
+
+                        if (confirm("确定要结束本次竞价吗？")) {
+                            activity[i].status = 4
+                            localStorage.setItem('activity', JSON.stringify(activity));
+                            $location.path("/competition")
+                        }
+                    }
                 }
             }
-//
         }
-    $scope.action=function(){
+        if (activity) {
+            for (var i = 0; i < activity.length; i++) {
+                if (activity[i].name == active_name) {
+                    if (activity[i].status == 3) {
 
-    }
-    })
+                        $scope.start = "结束"
+                    }
+                    else {
+                        $scope.start = "开始"
+                        $scope.usable = true
+                    }
+//                    var unable = true
+//                    break
+                }
+//                else {
+//                    unable = false
+//                }
+//                if (unable == false) {
+//                    $scope.start_end = "开始"
+//                    $scope.usable = false
+//                }
+            }
+////
+            }
+        })
